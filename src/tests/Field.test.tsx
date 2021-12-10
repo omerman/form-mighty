a7015332f624fe7d100b50e17c58b598dd89352f
Omer Prizner <omer.prizner@gmail.com>
2021-12-10 16:14:19 +0200

    Made Field dirty indicator tests neat and clean, so we will be able to add more tests at ease.

--- src/tests/Field.test.tsx
+++ src/tests/Field.test.tsx
@@ -127,208 +127,237 @@ describe("dirty indicator", () => {
     expect(container.querySelector("code")).toHaveTextContent("false");
   });
 
-  it("should change after value changes", async () => {
-    const { container } = render(
-      <FormProvider>
-        <FormMighty initialValues={{ value: 5 }}>
-          {(tk) => (
-            <Field fieldPath={tk.path("value")}>
-              {({ onChange }, isDirty) => (
-                <code onClick={() => onChange(1000)}>{String(isDirty)}</code>
-              )}
-            </Field>
-          )}
-        </FormMighty>
-      </FormProvider>
-    );
+  describe("leaf", () => {
+    it("should become dirty if field changes", async () => {
+      const { container } = render(
+        <FormProvider>
+          <FormMighty initialValues={{ value: 5 }}>
+            {(tk) => (
+              <Field fieldPath={tk.path("value")}>
+                {({ onChange }, isDirty) => (
+                  <code onClick={() => onChange(1000)}>{String(isDirty)}</code>
+                )}
+              </Field>
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
 
-    userEvent.click(container.querySelector("code")!);
+      userEvent.click(container.querySelector("code")!);
 
-    expect(container.querySelector("code")).toHaveTextContent("true");
+      expect(container.querySelector("code")).toHaveTextContent("true");
+    });
+
+    it("should become undirty if field is restored to initial value", async () => {
+      const { container } = render(
+        <FormProvider>
+          <FormMighty initialValues={{ value: "" }}>
+            {(tk) => (
+              <Field fieldPath={tk.path("value")}>
+                {({ value, onChange }, isDirty) => (
+                  <div>
+                    <input onChange={onChange} value={value} />
+                    <span>{String(isDirty)}</span>
+                  </div>
+                )}
+              </Field>
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
+
+      userEvent.type(container.querySelector("input")!, "H");
+      userEvent.clear(container.querySelector("input")!);
+
+      expect(container.querySelector("span")).toHaveTextContent("false");
+    });
+
+    it("should be dirty if field is new", () => {
+      type MyForm = { a?: { b: number } };
+
+      const { container } = render(
+        <FormProvider>
+          <FormMighty<MyForm> initialValues={{}}>
+            {(tk) => (
+              <Field fieldPath={tk.path("a.b")}>
+                {({ value, onChange }, _) => (
+                  <div>
+                    <code onClick={() => onChange(1000)} />
+                    <span>
+                      {String(tk.isFieldDirty("a.b"))} {value}
+                    </span>
+                  </div>
+                )}
+              </Field>
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
+
+      userEvent.click(container.querySelector("code")!);
+
+      expect(container.querySelector("span")).toHaveTextContent("true");
+    });
+
+    it("should be dirty if parent object value becomes undefined", async () => {
+      type MyForm = { a?: { b: number } };
+
+      const { container } = render(
+        <FormProvider>
+          <FormMighty<MyForm> initialValues={{ a: { b: 5 } }}>
+            {(tk) => (
+              <Field fieldPath={tk.path("a")}>
+                {({ value, onChange }, _) => (
+                  <div>
+                    <code onClick={() => onChange(undefined)} />
+                    <span>{String(tk.isFieldDirty("a.b"))}</span>
+                  </div>
+                )}
+              </Field>
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
+
+      userEvent.click(container.querySelector("code")!);
+
+      expect(container.querySelector("span")).toHaveTextContent("true");
+    });
+
+    it("should be dirty if parent array value becomes undefined", async () => {
+      type MyForm = { a?: string[] };
+
+      const { container } = render(
+        <FormProvider>
+          <FormMighty<MyForm> initialValues={{ a: ["5"] }}>
+            {(tk) => (
+              <Field fieldPath={tk.path("a")}>
+                {({ value, onChange }, _) => (
+                  <div>
+                    <code onClick={() => onChange(undefined)} />
+                    <span>{String(tk.isFieldDirty("a.0"))}</span>
+                  </div>
+                )}
+              </Field>
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
+
+      userEvent.click(container.querySelector("code")!);
+
+      expect(container.querySelector("span")).toHaveTextContent("true");
+    });
   });
 
-  it("should change back after value becomes original", async () => {
-    const { container } = render(
-      <FormProvider>
-        <FormMighty initialValues={{ value: "" }}>
-          {(tk) => (
-            <Field fieldPath={tk.path("value")}>
-              {({ value, onChange }, isDirty) => (
-                <div>
-                  <input onChange={onChange} value={value} />
-                  <span>{String(isDirty)}</span>
-                </div>
-              )}
-            </Field>
-          )}
-        </FormMighty>
-      </FormProvider>
-    );
+  describe("parent object", () => {
+    it("should become dirty if child's field changes", () => {
+      const { container } = render(
+        <FormProvider>
+          <FormMighty initialValues={{ value: { nestedValue: 5 } }}>
+            {(tk) => (
+              <Field fieldPath={tk.path("value.nestedValue")}>
+                {({ onChange }, _) => (
+                  <code onClick={() => onChange(1000)}>
+                    {String(tk.isFieldDirty("value"))}
+                  </code>
+                )}
+              </Field>
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
 
-    userEvent.type(container.querySelector("input")!, "H");
-    userEvent.clear(container.querySelector("input")!);
+      userEvent.click(container.querySelector("code")!);
 
-    expect(container.querySelector("span")).toHaveTextContent("false");
-  });
+      expect(container.querySelector("code")).toHaveTextContent("true");
+    });
 
-  it("should also mark parent of nested path", () => {
-    const { container } = render(
-      <FormProvider>
-        <FormMighty initialValues={{ value: { nestedValue: 5 } }}>
-          {(tk) => (
-            <Field fieldPath={tk.path("value.nestedValue")}>
-              {({ onChange }, _) => (
-                <code onClick={() => onChange(1000)}>
-                  {String(tk.isFieldDirty("value"))}
-                </code>
-              )}
-            </Field>
-          )}
-        </FormMighty>
-      </FormProvider>
-    );
-
-    userEvent.click(container.querySelector("code")!);
-
-    expect(container.querySelector("code")).toHaveTextContent("true");
-  });
-
-  it("should change back parent of nested path after value becomes original", async () => {
-    const { container } = render(
-      <FormProvider>
-        <FormMighty initialValues={{ value: { nestedValue: "" } }}>
-          {(tk) => (
-            <Field fieldPath={tk.path("value.nestedValue")}>
-              {({ value, onChange }, _) => (
-                <div>
-                  <input onChange={onChange} value={value} />
-                  <span>{String(tk.isFieldDirty("value"))}</span>
-                </div>
-              )}
-            </Field>
-          )}
-        </FormMighty>
-      </FormProvider>
-    );
-
-    userEvent.type(container.querySelector("input")!, "H");
-    userEvent.clear(container.querySelector("input")!);
-
-    expect(container.querySelector("span")).toHaveTextContent("false");
-  });
-
-  it("should not change back parent of nested path after sibling value becomes original", async () => {
-    const { container } = render(
-      <FormProvider>
-        <FormMighty
-          initialValues={{ value: { nestedValue: "", nestedValue2: "" } }}
-        >
-          {(tk) => (
-            <>
+    it("should become undirty if child's field is restored to initial value", () => {
+      const { container } = render(
+        <FormProvider>
+          <FormMighty initialValues={{ value: { nestedValue: "" } }}>
+            {(tk) => (
               <Field fieldPath={tk.path("value.nestedValue")}>
                 {({ value, onChange }, _) => (
                   <div>
-                    <input id="input1" onChange={onChange} value={value} />
+                    <input onChange={onChange} value={value} />
                     <span>{String(tk.isFieldDirty("value"))}</span>
                   </div>
                 )}
               </Field>
-              <Field fieldPath={tk.path("value.nestedValue2")}>
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
+
+      userEvent.type(container.querySelector("input")!, "H");
+      userEvent.clear(container.querySelector("input")!);
+
+      expect(container.querySelector("span")).toHaveTextContent("false");
+    });
+
+    it("should remain dirty if only one child's field is restored to initial value", () => {
+      const { container } = render(
+        <FormProvider>
+          <FormMighty
+            initialValues={{ value: { nestedValue: "", nestedValue2: "" } }}
+          >
+            {(tk) => (
+              <>
+                <Field fieldPath={tk.path("value.nestedValue")}>
+                  {({ value, onChange }, _) => (
+                    <div>
+                      <input id="input1" onChange={onChange} value={value} />
+                      <span>{String(tk.isFieldDirty("value"))}</span>
+                    </div>
+                  )}
+                </Field>
+                <Field fieldPath={tk.path("value.nestedValue2")}>
+                  {({ value, onChange }, _) => (
+                    <div>
+                      <input id="input2" onChange={onChange} value={value} />
+                      <span>{String(tk.isFieldDirty("value"))}</span>
+                    </div>
+                  )}
+                </Field>
+              </>
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
+
+      userEvent.type(container.querySelector("#input1")!, "H");
+      userEvent.type(container.querySelector("#input2")!, "H");
+
+      userEvent.clear(container.querySelector("#input1")!);
+
+      expect(container.querySelector("span")).toHaveTextContent("true");
+    });
+
+    it("should be dirty if child's field is new", () => {
+      type MyForm = { a?: { b: number } };
+
+      const { container } = render(
+        <FormProvider>
+          <FormMighty<MyForm> initialValues={{}}>
+            {(tk) => (
+              <Field fieldPath={tk.path("a.b")}>
                 {({ value, onChange }, _) => (
                   <div>
-                    <input id="input2" onChange={onChange} value={value} />
-                    <span>{String(tk.isFieldDirty("value"))}</span>
+                    <code onClick={() => onChange(1000)} />
+                    <span>{String(tk.isFieldDirty("a"))}</span>
                   </div>
                 )}
               </Field>
-            </>
-          )}
-        </FormMighty>
-      </FormProvider>
-    );
+            )}
+          </FormMighty>
+        </FormProvider>
+      );
 
-    userEvent.type(container.querySelector("#input1")!, "H");
-    userEvent.type(container.querySelector("#input2")!, "H");
+      userEvent.click(container.querySelector("code")!);
 
-    userEvent.clear(container.querySelector("#input1")!);
-
-    expect(container.querySelector("span")).toHaveTextContent("true");
-  });
-
-  it("should be true for added fields", async () => {
-    type MyForm = { a?: { b: number } };
-
-    const { container } = render(
-      <FormProvider>
-        <FormMighty<MyForm> initialValues={{}}>
-          {(tk) => (
-            <Field fieldPath={tk.path("a.b")}>
-              {({ value, onChange }, _) => (
-                <div>
-                  <code onClick={() => onChange(1000)} />
-                  <span>
-                    {String(tk.isFieldDirty("a.b"))} {value}
-                  </span>
-                </div>
-              )}
-            </Field>
-          )}
-        </FormMighty>
-      </FormProvider>
-    );
-
-    userEvent.click(container.querySelector("code")!);
-
-    expect(container.querySelector("span")).toHaveTextContent("true");
-  });
-
-  it("should be true for added fields parents as well", async () => {
-    type MyForm = { a?: { b: number } };
-
-    const { container } = render(
-      <FormProvider>
-        <FormMighty<MyForm> initialValues={{}}>
-          {(tk) => (
-            <Field fieldPath={tk.path("a.b")}>
-              {({ value, onChange }, _) => (
-                <div>
-                  <code onClick={() => onChange(1000)} />
-                  <span>{String(tk.isFieldDirty("a"))}</span>
-                </div>
-              )}
-            </Field>
-          )}
-        </FormMighty>
-      </FormProvider>
-    );
-
-    userEvent.click(container.querySelector("code")!);
-
-    expect(container.querySelector("span")).toHaveTextContent("true");
-  });
-
-  it("should be true for a child that it's parent value becomes undefined", async () => {
-    type MyForm = { a?: { b: number } };
-
-    const { container } = render(
-      <FormProvider>
-        <FormMighty<MyForm> initialValues={{ a: { b: 5 } }}>
-          {(tk) => (
-            <Field fieldPath={tk.path("a")}>
-              {({ value, onChange }, _) => (
-                <div>
-                  <code onClick={() => onChange(undefined)} />
-                  <span>{String(tk.isFieldDirty("a.b"))}</span>
-                </div>
-              )}
-            </Field>
-          )}
-        </FormMighty>
-      </FormProvider>
-    );
-
-    userEvent.click(container.querySelector("code")!);
-
-    expect(container.querySelector("span")).toHaveTextContent("true");
+      expect(container.querySelector("span")).toHaveTextContent("true");
+    });
   });
 });
