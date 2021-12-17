9cb6a2ebd3e1dcb2386ae32ee9e1adcb49e47504
Omer Prizner <omer.prizner@gmail.com>
2021-12-17 22:47:50 +0200

    remove redux devtools usage. no need.

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,9 +1,4 @@
 import { createStore } from "redux";
 import { reducer } from "./reducer";
 
-export const store = createStore(
-  reducer,
-  // TODO - REMOVE THIS
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-);
+export const store = createStore(reducer);
