4272bd65bf6df7246fb592fd08f4eded500cef87
Omer Prizner <omer.prizner@gmail.com>
2021-12-20 21:31:52 +0200

    useFormSubscription hook, which is very cool and gives you super powers.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,6 +4,7 @@ export { FormProvider } from "./FormProvider";
 export { FormMighty } from "./FormMighty";
 export { FormSubscriber } from "./FormSubscriber";
 export { useInitForm } from "./useInitForm";
+export { useFormSubscription } from "./useFormSubscription";
 export type { FormState, FormToolkitOptions } from "./types";
 
 enablePatches();
---
+++ src/tests/useFormSubscription.test.tsx
@@ -0,0 +1,132 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { FormMighty, FormProvider } from "src/lib";
+import { FormToolkit } from "src/lib/FormToolkit";
+import { useFormSubscription } from "src/lib/useFormSubscription";
+
+it("should work", () => {
+  const Wrapper: React.FC = ({ children }) => {
+    return <FormProvider>{children}</FormProvider>;
+  };
+
+  const { result } = renderHook(() => useFormSubscription(new FormToolkit()), {
+    wrapper: Wrapper,
+  });
+
+  expect(result.error).not.toBeDefined();
+});
+
+it("should return the entire form state if passing a toolkit instance", () => {
+  const Wrapper: React.FC = ({ children }) => {
+    return <FormProvider>{children}</FormProvider>;
+  };
+
+  const tk = new FormToolkit();
+  const { result } = renderHook(() => useFormSubscription(tk), {
+    wrapper: Wrapper,
+  });
+
+  expect(result.current).toBe(tk.getState());
+});
+
+it("should use the toolkit from context if not passed", () => {
+  const tk = new FormToolkit({ initialValues: { value: 5 } });
+
+  const Wrapper: React.FC = ({ children }) => {
+    return (
+      <FormProvider>
+        <FormMighty toolkit={tk}>{children}</FormMighty>
+      </FormProvider>
+    );
+  };
+
+  const { result } = renderHook(() => useFormSubscription(), {
+    wrapper: Wrapper,
+  });
+
+  expect(result.current).toBe(tk.getState());
+});
+
+it("should return a reactive form state", async () => {
+  const tk = new FormToolkit({
+    initialValues: { value: 1 },
+  });
+
+  const Wrapper: React.FC = ({ children }) => {
+    return <FormProvider>{children}</FormProvider>;
+  };
+
+  const { result } = renderHook(() => useFormSubscription(tk), {
+    wrapper: Wrapper,
+  });
+
+  tk.updateValues((draft) => {
+    draft.value = 2;
+  });
+
+  expect(result.current).toBe(tk.getState());
+});
+
+it("should return a form state using a given subscriptionFn", async () => {
+  const tk = new FormToolkit({
+    initialValues: { value: 1 },
+  });
+
+  const Wrapper: React.FC = ({ children }) => {
+    return <FormProvider>{children}</FormProvider>;
+  };
+
+  const { result } = renderHook(
+    () => useFormSubscription(tk, (state) => state.values.value),
+    {
+      wrapper: Wrapper,
+    }
+  );
+
+  expect(result.current).toBe(tk.getState().values.value);
+});
+
+it("should accept subscriptionFn as first argument and use context's toolkit", async () => {
+  const tk = new FormToolkit({
+    initialValues: { value: 1 },
+  });
+
+  const Wrapper: React.FC = ({ children }) => {
+    return (
+      <FormProvider>
+        <FormMighty toolkit={tk}>{children}</FormMighty>
+      </FormProvider>
+    );
+  };
+
+  const { result } = renderHook(
+    () => useFormSubscription((state) => state.values.value),
+    {
+      wrapper: Wrapper,
+    }
+  );
+
+  expect(result.current).toBe(tk.getState().values.value);
+});
+
+it("should not trigger render if result equals shallowly", async () => {
+  const tk = new FormToolkit({
+    initialValues: { value: 1 },
+  });
+
+  const Wrapper: React.FC = ({ children }) => {
+    return <FormProvider>{children}</FormProvider>;
+  };
+
+  const { result } = renderHook(
+    () => useFormSubscription(tk, (state) => ({ ...state.initialValues })),
+    {
+      wrapper: Wrapper,
+    }
+  );
+
+  tk.updateValues((draft) => {
+    draft.value = 2;
+  });
+
+  expect(result.all.length).toBe(1);
+});
