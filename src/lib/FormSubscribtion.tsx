89ed12b9cba3ed5e2bb27e485b8b46080a62062a
Omer Prizner <omer.prizner@gmail.com>
2022-01-09 22:18:44 +0200

    FormSubscription - removed most of the login, fully test the remaining logic. make it only wrap the useFormSubscription hook, nothing more.

--- src/lib/FormSubscribtion.tsx
+++ src/lib/FormSubscribtion.tsx
@@ -1,27 +1,19 @@
 import { FormState, DefaultFormValues } from "./types";
 import invariant from "invariant";
-import { useEffect, useRef } from "react";
 import { FormToolkit } from "./FormToolkit";
 import { useForm } from "./useForm";
 import { useFormSubscription } from "./useFormSubscription";
 
 export interface FormSubscribtionProps<V extends DefaultFormValues, T> {
   subscription: (state: FormState<V>) => T;
-  onMount?: (subscriptonResult: T, toolkit: FormToolkit<V>) => void;
-  onChange?: (subscriptonResult: T, toolkit: FormToolkit<V>) => void;
-  children?: (subscriptonResult: T, toolkit: FormToolkit<V>) => React.ReactNode;
+  children: (subscriptonResult: T, toolkit: FormToolkit<V>) => React.ReactNode;
 }
 
 export const FormSubscribtion = <V extends DefaultFormValues, T>({
   subscription,
-  onMount,
-  onChange,
   children,
 }: FormSubscribtionProps<V, T>) => {
-  invariant(
-    onMount ?? onChange ?? children,
-    "FormSubscribtion - Must include one of [onMount, onChange, children] props"
-  );
+  invariant(children, "FormSubscribtion - Must include children");
   const formToolkit = useForm<V>();
 
   const subscriptonResult = useFormSubscription<V, T>(
@@ -29,17 +21,5 @@ export const FormSubscribtion = <V extends DefaultFormValues, T>({
     subscription
   );
 
-  const mountedRef = useRef(false);
-  const refs = useRef({ onChange, onMount });
-
-  useEffect(() => {
-    if (mountedRef.current) {
-      refs.current.onChange?.(subscriptonResult, formToolkit);
-    } else {
-      mountedRef.current = true;
-      refs.current.onMount?.(subscriptonResult, formToolkit);
-    }
-  }, [formToolkit, subscriptonResult]);
-
-  return <>{children?.(subscriptonResult, formToolkit) ?? null}</>;
+  return <>{children(subscriptonResult, formToolkit)}</>;
 };
---
+++ src/tests/FormSubscription.test.tsx
@@ -0,0 +1,136 @@
+import { render } from "@testing-library/react";
+import { FormProvider } from "src/lib/FormProvider";
+import { FormMighty } from "src/lib/FormMighty";
+import { FormState, FormSubscribtion } from "src/lib";
+import { FormToolkit } from "src/lib/FormToolkit";
+
+it("should render", () => {
+  render(
+    <FormProvider>
+      <FormMighty initialValues={{}}>
+        <FormSubscribtion subscription={() => ({})}>
+          {() => null}
+        </FormSubscribtion>
+      </FormMighty>
+    </FormProvider>
+  );
+});
+
+it("should throw if children not present", () => {
+  const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {});
+
+  expect(() =>
+    render(
+      <FormProvider>
+        <FormMighty initialValues={{}}>
+          {/* @ts-ignore */}
+          <FormSubscribtion subscription={() => ({})} />
+        </FormMighty>
+      </FormProvider>
+    )
+  ).toThrow();
+
+  consoleSpy.mockRestore();
+});
+
+it("should pass subscription result uppon render children", () => {
+  type MyForm = { field: number };
+  const { container } = render(
+    <FormProvider>
+      <FormMighty<MyForm> initialValues={{ field: 5 }}>
+        <FormSubscribtion
+          subscription={(state: FormState<MyForm>) => ({
+            res: state.values,
+          })}
+        >
+          {({ res }) => <code>{res.field}</code>}
+        </FormSubscribtion>
+      </FormMighty>
+    </FormProvider>
+  );
+
+  expect(container.querySelector("code")).toHaveTextContent(/^5$/);
+});
+
+it("should re-render if subscription field is unchanged", () => {
+  type MyForm = { field: number };
+  const tk = new FormToolkit<MyForm>({
+    initialValues: { field: 5 },
+  });
+
+  const { container } = render(
+    <FormProvider>
+      <FormMighty toolkit={tk}>
+        <FormSubscribtion
+          subscription={(state: FormState<MyForm>) => ({
+            field: state.values.field,
+          })}
+        >
+          {({ field }) => <code>{field}</code>}
+        </FormSubscribtion>
+      </FormMighty>
+    </FormProvider>
+  );
+
+  tk.updateValues((draft) => {
+    draft.field = 1000;
+  });
+
+  expect(container.querySelector("code")).toHaveTextContent(/^1000$/);
+});
+
+it("should not re-render if subscription field is unchanged", () => {
+  const renderChecker = jest
+    .fn()
+    .mockImplementationOnce(() => "Once")
+    .mockImplementationOnce(() => "Twice");
+
+  type MyForm = { field1: number; field2: number };
+  const tk = new FormToolkit<MyForm>({
+    initialValues: { field1: 5, field2: 5 },
+  });
+
+  const { container } = render(
+    <FormProvider>
+      <FormMighty toolkit={tk}>
+        <FormSubscribtion
+          subscription={(state: FormState<MyForm>) => ({
+            field1: state.values.field1,
+          })}
+        >
+          {() => (
+            <>
+              <code>{renderChecker()}</code>
+            </>
+          )}
+        </FormSubscribtion>
+      </FormMighty>
+    </FormProvider>
+  );
+
+  tk.updateValues((draft) => {
+    draft.field2 = 1000;
+  });
+
+  expect(container.querySelector("code")).toHaveTextContent(/^Once$/);
+});
+
+it("should pass toolkit instance uppon render children as second argument", () => {
+  const acceptFormToolkitFn = jest.fn();
+  const tk = new FormToolkit();
+
+  render(
+    <FormProvider>
+      <FormMighty toolkit={tk}>
+        <FormSubscribtion subscription={() => null}>
+          {(_, tk) => {
+            acceptFormToolkitFn(tk);
+            return null;
+          }}
+        </FormSubscribtion>
+      </FormMighty>
+    </FormProvider>
+  );
+
+  expect(acceptFormToolkitFn).toHaveBeenCalledWith(tk);
+});
